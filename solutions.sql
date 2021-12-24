239ff92df5dc475c8fb0726c5401901f65fe8564
jecastrom <jcastro.de@outlook.com>
2021-12-24 02:25:03 +0100

    w

--- solutions.sql
+++ solutions.sql
@@ -1,5 +1,6 @@
 /*
- Instructions
+ Instructions:      
+ 
  Assume that any _id columns are incremental, meaning that higher ids always occur
  after lower ids. For example, a client with a higher client_id joined the bank after 
  a client with a lower client_id.
