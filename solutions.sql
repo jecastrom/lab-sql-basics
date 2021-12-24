9fa7e323b19ded458069ebab411b327e0a1dece3
jecastrom <jcastro.de@outlook.com>
2021-12-24 02:24:05 +0100

    w

---
+++ solutions.sql
@@ -0,0 +1,9 @@
+/*
+ Instructions
+ Assume that any _id columns are incremental, meaning that higher ids always occur
+ after lower ids. For example, a client with a higher client_id joined the bank after 
+ a client with a lower client_id.
+ 
+ Query 1
+ Get the id values of the first 5 clients from district_id with a value equals to 1.
+ */
